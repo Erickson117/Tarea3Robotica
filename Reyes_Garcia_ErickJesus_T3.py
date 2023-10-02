b6db027d60f115d47732e8d768c40f39179c2a62
Erickson117 <erickreyes07@aragon.unam.mx>
2023-10-02 12:39:53 -0600

    Tarea en .py

---
+++ Reyes_Garcia_ErickJesus_T3.py
@@ -0,0 +1,34 @@
+#Alumno: Reyes Garcia Erick Jesus
+#Grupo: 1058
+#Asignatura: Robotica.
+
+import math
+#math.degrees()
+
+def calcularXY(theta1,theta2):
+  #Longitudes de ambos ejes
+  l1 = 1.3
+  l2 = 0.8
+  #Calcular X y Y
+  x = (l1*math.degrees(math.cos(theta1))) + (l2*math.degrees(math.cos(theta1 + theta2)))
+  y = (l1*math.degrees(math.sin(theta1))) + (l2*math.degrees(math.sin(theta1 + theta2)))
+  return f"({x},{y})" #valor a regresar
+
+#Ingresar los angulos desde el teclado y comprobando que se metan numeros
+error = 0 #esta variable la uso para evitar que se impriman las coordenadas con los datos en memoria cuando se introducen caracteres invalidos
+
+try:
+  theta1 = int(input("Ingresa tu valor de theta1: "))
+  theta2 = int(input("Ingresa tu valor de theta2: "))
+except:
+  print("Estas introduciendo valores no validos")
+  error += 1
+
+
+#comprobar que solo se muevan en el rango permitido en este caso el primero va de 0 a 180 y el segundo de 0 a 90
+if(theta1 < 0 or theta1 > 180 ):
+  print("Valores no validos para theta 1")
+elif(theta2 < 0 or theta2 > 90):
+  print("Valores no validos para theta 2")
+elif(error == 0):
+  print("Las coordenadas del efector final se encuentran en: "+calcularXY(theta1,theta2)) #Imprime el resultado
